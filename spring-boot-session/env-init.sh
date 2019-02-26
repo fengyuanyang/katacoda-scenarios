b100935ff69ca6d9c64139437d2fb914a0aa1e56
owen <coolsealtw@hotmail.com>
2019-02-26 15:54:22 +0800

    modify env-init.sh

--- spring-boot-session/env-init.sh
+++ spring-boot-session/env-init.sh
@@ -1,7 +1,7 @@
-PROJECT=spring-mvc	  # The name of the folder within the code samples repo to copy
+PROJECT=spring-session	  # The name of the folder within the code samples repo to copy
 UI_PATH=/root/code 	  # This should match your index.json key
 
 git clone -q https://github.com/spring-projects/spring-session.git
-cd ${UI_PATH} && cp -R /root/rhoar-sample-code/${PROJECT}/* ./
+cd ${UI_PATH} && cp -R /root/${PROJECT}/* ./
 clear # To clean up Katacoda terminal noise
 ~/.launch.sh
