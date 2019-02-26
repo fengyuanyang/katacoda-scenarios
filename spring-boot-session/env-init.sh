60f51e3e5d69b80058f248761639d32379561928
owen <coolsealtw@hotmail.com>
2019-02-26 18:57:15 +0800

    add env-init

--- spring-boot-session/env-init.sh
+++ spring-boot-session/env-init.sh
@@ -1,7 +1,9 @@
 PROJECT=spring-session	  # The name of the folder within the code samples repo to copy
 UI_PATH=/root/code 	  # This should match your index.json key
 
-git clone -q https://github.com/spring-projects/spring-session.git
+git clone https://github.com/spring-projects/spring-session.git
+git checkout 2.1.4.RELEASE
+#https://github.com/spring-projects/spring-session.git
 cd ${UI_PATH} && cp -R /root/${PROJECT}/* ./
 clear # To clean up Katacoda terminal noise
 ~/.launch.sh
